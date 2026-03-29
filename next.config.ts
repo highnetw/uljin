545f19606c3c014a118bfef4add06b5970665bbd
BI Woo <chooyoonkim@gmail.com>
2026-03-30 02:31:40 +0900

    fix: remove next-pwa, fix turbopack config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 import type { NextConfig } from 'next'
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-})
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   images: {
-    domains: ['umvefycoasglvvujmfmb.supabase.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'umvefycoasglvvujmfmb.supabase.co',
+      },
+    ],
   },
 }
 
-module.exports = withPWA(nextConfig)
+export default nextConfig
